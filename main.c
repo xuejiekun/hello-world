5ecf4b9161b7214ee1bc0cc7ec62a5f7726e8e43
xuejiekun <512559733@qq.com>
2018-07-10 08:23:53 +0800

    print double

--- main.c
+++ main.c
@@ -4,9 +4,11 @@ int main(void)
 {
     int a = 10;
     float b = 10.0/3.0;
+    double c = 10.0/3.0;
 
 	printf("hello world!\n");
 	printf("print int:%d.\n", a);
 	printf("print float:%f.\n", b);
+	printf("print double:%f.\n", c);
 	return 0;
 }
